0f977edd6418d4a23c4bf866b33af20f42cc2666
Juergen Enge <you@example.com>
2024-08-02 18:39:42 +0200

    minivaultloader additions

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.5
 
 require (
 	emperror.dev/errors v0.8.1
+	github.com/je4/minivault/v2 v2.0.0-20240802094140-b281a9263048
 	github.com/je4/trustutil/v2 v2.0.19
 	github.com/je4/utils/v2 v2.0.41
 	github.com/smallstep/certinfo v1.12.2
@@ -11,15 +12,55 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/bytedance/sonic v1.11.9 // indirect
+	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/deneonet/benc v1.0.9 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
+	github.com/gin-contrib/cors v1.7.2 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.10.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
-	github.com/google/certificate-transparency-go v1.1.7 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.21.0 // indirect
+	github.com/go-openapi/spec v0.21.0 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.22.0 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/google/certificate-transparency-go v1.2.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rs/zerolog v1.33.0 // indirect
-	go.step.sm/crypto v0.46.0 // indirect
+	github.com/swaggo/files v1.0.1 // indirect
+	github.com/swaggo/gin-swagger v1.6.0 // indirect
+	github.com/swaggo/swag v1.16.3 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
+	go.step.sm/crypto v0.48.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
+	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
-	k8s.io/klog/v2 v2.100.1 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/tools v0.22.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/klog/v2 v2.130.1 // indirect
 )
--- pkg/loader/miniVaultLoader.go
+++ pkg/loader/miniVaultLoader.go
@@ -1,19 +1,27 @@
 package loader
 
 import (
+	"bytes"
 	"crypto/tls"
 	"crypto/x509"
 	"emperror.dev/errors"
+	"encoding/json"
+	vaultRest "github.com/je4/minivault/v2/pkg/rest"
 	"github.com/je4/utils/v2/pkg/zLogger"
+	"io"
 	"net/http"
+	"sync"
 	"time"
 )
 
-func NewMiniVaultLoader(baseURL, token string, tokenInterval, certInterval time.Duration, ca *x509.Certificate, logger zLogger.ZLogger) (*MiniVaultLoader, error) {
+func NewMiniVaultLoader(baseURL, token string, certType string, uris, dnss []string, tokenInterval, certInterval time.Duration, ca *x509.Certificate, logger zLogger.ZLogger) (*MiniVaultLoader, error) {
 	l := &MiniVaultLoader{
 		baseURL:       baseURL,
 		certChannel:   make(chan *tls.Certificate),
 		token:         token,
+		certType:      certType,
+		uris:          uris,
+		dnss:          dnss,
 		certInterval:  certInterval,
 		tokenInterval: tokenInterval,
 		done:          make(chan bool),
@@ -40,6 +48,7 @@ func NewMiniVaultLoader(baseURL, token string, tokenInterval, certInterval time.
 }
 
 type MiniVaultLoader struct {
+	tokenMutex    sync.Mutex
 	client        *http.Client
 	certChannel   chan *tls.Certificate
 	caCertPool    *x509.CertPool
@@ -50,8 +59,137 @@ type MiniVaultLoader struct {
 	logger        zLogger.ZLogger
 	token         string
 	baseURL       string
+	certType      string
+	uris          []string
+	dnss          []string
 }
 
-func (f *MiniVaultLoader) GetCA() *x509.CertPool {
-	f.client.Get(f.baseURL + "/cert/ca/pem")
+func (f *MiniVaultLoader) setToken(token string) {
+	f.tokenMutex.Lock()
+	f.tokenMutex.Unlock()
+	f.token = token
 }
+
+func (f *MiniVaultLoader) getToken() string {
+	f.tokenMutex.Lock()
+	f.tokenMutex.Unlock()
+	return f.token
+}
+
+func (f *MiniVaultLoader) getCert() *tls.Certificate {
+	param := &vaultRest.CertRequestMessage{}
+	data, err := json.Marshal(param)
+	if err != nil {
+		f.logger.Error().Err(err).Msg("cannot marshal request")
+	}
+	req, err := http.NewRequest("POST", f.baseURL+"/cert/create", bytes.NewBuffer(data))
+	if err != nil {
+		f.logger.Error().Err(err).Msg("cannot create request")
+		return nil
+	}
+	req.Header.Set("X-Vault-Token", f.getToken())
+	resp, err := f.client.Do(req)
+	if err != nil {
+		f.logger.Error().Err(err).Msg("cannot get cert")
+		return nil
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		if result, err := io.ReadAll(resp.Body); err != nil {
+			f.logger.Error().Msgf("cannot get cert: %s - %s", resp.Status, string(result))
+		} else {
+			f.logger.Error().Msgf("cannot get cert: %s", resp.Status)
+		}
+		return nil
+	}
+	result := vaultRest.CertResultMessage{}
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		f.logger.Error().Err(err).Msg("cannot decode cert")
+		return nil
+	}
+	cert, err := tls.X509KeyPair([]byte(result.Cert), []byte(result.Key))
+	if err != nil {
+		f.logger.Error().Err(err).Msg("cannot parse cert")
+		return nil
+	}
+	return &cert
+}
+
+func (f *MiniVaultLoader) Close() error {
+	f.done <- true
+	close(f.certChannel)
+	return nil
+}
+
+func (f *MiniVaultLoader) Run() error {
+	var tokenDone = make(chan bool)
+	var certDone = make(chan bool)
+	var wg = sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-tokenDone:
+				return
+			case <-time.After(f.tokenInterval):
+			}
+
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-certDone:
+				return
+			case <-time.After(f.certInterval):
+			}
+		}
+	}()
+	select {
+	case <-f.done:
+	}
+	close(tokenDone)
+	close(certDone)
+	wg.Wait()
+	return nil
+}
+
+func (f *MiniVaultLoader) getCA() *x509.CertPool {
+	req, err := http.NewRequest("GET", f.baseURL+"/cert/ca/pem", nil)
+	if err != nil {
+		f.logger.Error().Err(err).Msg("cannot create request")
+		return nil
+	}
+	req.Header.Set("X-Vault-Token", f.token)
+	resp, err := f.client.Do(req)
+	if err != nil {
+		f.logger.Error().Err(err).Msg("cannot get ca")
+		return nil
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		if result, err := io.ReadAll(resp.Body); err != nil {
+			f.logger.Error().Msgf("cannot get ca: %s - %s", resp.Status, string(result))
+		} else {
+			f.logger.Error().Msgf("cannot get ca: %s", resp.Status)
+		}
+		return nil
+	}
+	result := vaultRest.CertResultMessage{}
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		f.logger.Error().Err(err).Msg("cannot decode ca")
+		return nil
+	}
+	caCert, err := x509.ParseCertificate([]byte(result.CA))
+	if err != nil {
+		f.logger.Error().Err(err).Msg("cannot parse ca")
+		return nil
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AddCert(caCert)
+	return caCertPool
+}
+
+var _ Loader = (*MiniVaultLoader)(nil)
